05755f3af0ffe6418bd41768605fab8ebeb40f76
Volodymyr Burkovskyi <v.burkovskyi@gamzix.com>
2022-05-27 20:03:28 +0300

    [script.js] created function for save selected in localStorage

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ const seats = document.querySelector('.row .seats:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
+
+let ticketPrice = +movieSelect.value;
+
+//Save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
