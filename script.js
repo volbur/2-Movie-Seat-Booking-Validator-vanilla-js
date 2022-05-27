9eb1befdb2879cc8bfc8f1ebd368edbb3fb19dad
Volodymyr Burkovskyi <v.burkovskyi@gamzix.com>
2022-05-27 20:14:59 +0300

    [script.js] created function for update total and count

--- script.js
+++ script.js
@@ -14,3 +14,18 @@ function setMovieData(movieIndex, moviePrice) {
     localStorage.setItem('selectedMoviePrice', moviePrice);
 }
 
+// Update total and count
+function updateSelectedCount() {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+    const seatIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
+    const selectedSeatsCount = selectedSeats.length;
+
+    count.innerText = selectedSeatsCount;
+    total.innerText = selectedSeatsCount * ticketPrice;
+
+    setMovieData(movieSelect.selectedIndex, movieSelect.value);
+}
