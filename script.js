11379ed0cea6cce9d46febf745df8ca9f1a05b79
Volodymyr Burkovskyi <v.burkovskyi@gamzix.com>
2022-05-27 19:55:25 +0300

    [script.js] addid refs

---
+++ script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const container = document.querySelector('.container');
+const seats = document.querySelector('.row .seats:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
